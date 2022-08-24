5d316850b1bfbe5b53bf93109be99f374709f282
Richard Clark <dcla839@auckland.uni.ac.nz>
2022-08-25 02:13:31 +1200

    mrf

--- public/script.js
+++ public/script.js
@@ -1,12 +1,28 @@
 window.addEventListener('load', () => {
-  const btn1 = document.querySelector('#enter');
-  const inputTxt = document.querySelector('#main-article-cmt-cmt')
-  btn1.addEventListener('click', () => {
-    const newComment = inputTxt.value;
-    console.log(newComment)
-    const newCommentEle = document.createElement('p')
-    newCommentEle.innerText=newComment
-    const insertSpot = document.querySelector('#comments-section');
-    insertSpot.appendChild(newCommentEle);
+  const inputTxt = document.querySelector('#main-article-cmt-cmt');
+  const article = document.querySelector('#article');
+  const newCommentSpace = document.createElement('ul');
+
+  
+  // create new button and inject into the DOM, add a class to it for styling 
+  const myBtn = document.createElement('button');
+  myBtn.innerText ="Add Comment";
+  document.body.appendChild(myBtn);
+  myBtn.classList.add('buttons');
+
+  // create a custom event
+  const myEvent = new Event('comment');
+
+  // listen for the event
+  myBtn.addEventListener('click', (e) => {
+    // const composed = e.composedPath()
+    // console.log(composed);
+    // console.log(composed.filter)
+    article.appendChild(newCommentSpace);
+    newCommentSpace.append(inputTxt.value)
+
+
   })
+  // dispatch the event
+  myBtn.dispatchEvent(myEvent)
 });
