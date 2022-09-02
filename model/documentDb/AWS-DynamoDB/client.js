880402efb8d1f768754bfe0cc59a62ca65b6eed8
Richard Clark <dcla839@aucklanduni.ac.nz>
2022-09-03 09:05:18 +1200

    dbfile updates

---
+++ model/documentDb/AWS-DynamoDB/client.js
@@ -0,0 +1,7 @@
+// Create the DynamoDB service client module using ES6 syntax.
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+// Set the AWS Region.
+export const REGION = "REGION"; // For example, "us-east-1".
+// Create an Amazon DynamoDB service client object.
+export const ddbClient = new DynamoDBClient({ region: REGION });
+
